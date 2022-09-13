9f0cf0adc517ba58ed4df5bbf88b3d98bd03daa7
juanmadls <juanmadls6@gmail.com>
2022-09-12 23:40:12 -0300

    modificacion en vistas

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,5 @@
 import { request, response, Router } from "express";
+import auth from "../lib/auth";
 
 
 const router = Router();
@@ -6,6 +7,10 @@ const router = Router();
 router.get("/", (request, response)  => {
   response.render("../views/home");
 });
+
+router.get("/profile", auth.isLoggedIn, (request,response) => {
+  response.render("../views/profile")
+})
 
 
 export { router };
