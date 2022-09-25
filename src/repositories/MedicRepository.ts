82bfaf0449379b139b2c7af43fc01a761a20e6e5
juanmadls <juanmadls6@gmail.com>
2022-09-25 13:06:50 -0300

    se quitan entidades que no se van a utilizar

--- src/repositories/MedicRepository.ts
+++
@@ -1,7 +0,0 @@
-import { Repository, EntityRepository } from "typeorm";
-import { Medic } from "../entities/Medic";
-
-@EntityRepository(Medic)
-class MedicRepository extends Repository<Medic>{ }
-
-export { MedicRepository };
